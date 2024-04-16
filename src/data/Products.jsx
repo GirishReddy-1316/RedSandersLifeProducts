6da43ff3d0c02c7b183541bcfe5e55876817e607
GiriGunduluruTR <127122389+GiriGunduluruTR@users.noreply.github.com>
2024-04-16 17:17:44 +0530

    jiyaba

--- src/data/Products.jsx
+++ src/data/Products.jsx
@@ -1,16 +1,3 @@
-import blackTea from "../assets/products/black.png";
-import whiteTea from "../assets/products/white.png";
-import yellowTea from "../assets/products/yellow.png";
-import blackAndWhiteTea from "../assets/products/blackandwhite.png";
-import blueTea from "../assets/products/blue.png";
-import blueberryTea from "../assets/products/blueberry.png";
-import brownTea from "../assets/products/brown.png";
-import chamomileTea from "../assets/products/chamomile.png";
-import coralTea from "../assets/products/coral.png";
-import ginsengTea from "../assets/products/ginseng.png";
-import indigoTea from "../assets/products/indigo.png";
-import limegreenTea from "../assets/products/limegreen.png";
-import magentaTea from "../assets/products/magenta.png";
 import neemTea from "../assets/products/neem.png";
 import orangeTea from "../assets/products/orange.png";
 import puerhTea from "../assets/products/puerh.png";
@@ -20,32 +7,32 @@ import turquoiseTea from "../assets/products/turquoise.png";
 const Products = [
   {
     id: 1,
-    brandName: "HRK",
+    brandName: "Jiyaba",
     image: turquoiseTea,
-    name: "Pure Red Sanders Water",
-    price: "₹750",
-    category: "Drink",
-    size: "3500 ml",
+    name: "Redtox Water",
+    price: "₹75",
+    category: "Bevarage",
+    size: "350 ml",
     featured: true,
-    slug: "pure-red-sanders-water",
+    slug: "redtox-water",
     desc: "Where does this come from: this water is extracted from the roots of the red sanders plant. It's a unique process we're using to get water from red sanders roots. By drinking this water, you can enjoy additional health benefits. Additionally, the regular water used in this process is purified RO drinking water.",
   },
 
   {
     id: 2,
-    brandName: "HRK",
+    brandName: "Jiyaba",
     image: redTea,
-    name: "Red Sanders Water",
-    price: "₹1000",
-    category: "Drink",
-    size: "3500 ml",
+    name: "Redjiya",
+    price: "₹100",
+    category: "Bevarage",
+    size: "350 ml",
     featured: true,
-    slug: "red-sanders-water",
+    slug: "red-jiya-water",
     desc: "Where does this come from: It's extracted from the roots of the red sanders plant using a special method. By drinking this water, you can enjoy extra health benefits along with a pleasant taste. Furthermore, the regular water used in this process is purified RO drinking water, and we've also added some natural sugars that are beneficial to our health.",
   },
   {
     id: 3,
-    brandName: "HRK",
+    brandName: "Jiyaba",
     image: puerhTea,
     name: "Pure Red Sanders Face Powder",
     price: "₹200",
@@ -54,31 +41,7 @@ const Products = [
     featured: true,
     slug: "red-sanders-face-powder",
     desc: "Where does this come from: It's obtained from the logs of the red sanders plant. We'll chop it into small pieces and then use machines to turn it into a fine powder. This fine powder can be applied directly to your skin by mixing it with rose water or regular water. As for its uses: it helps in preventing tanning, aging, and moisturizing the skin. It also brightens the skin tone, reduces pigmentation, fights acne, protects the skin, and soothes burns.",
-  },
-  {
-    id: 4,
-    brandName: "HRK",
-    image: orangeTea,
-    name: "Red Sanders Lip Balm",
-    price: "₹200",
-    category: "gel",
-    size: "15 gms",
-    featured: true,
-    slug: "red-lip-balm",
-    desc: "Where does this come from: This is very natural product we made it specially for you using bees wax and red sanders fine powder",
-  },
-  {
-    id: 5,
-    brandName: "HRK",
-    image: neemTea,
-    name: "Red Sanders Moisturizer",
-    price: "₹200",
-    category: "gel",
-    size: "150 gms",
-    featured: true,
-    slug: "red-moisturizer",
-    desc: "Where does this come from: This is very natural product we made it specially for you using some natural products",
-  },
+  }
 ];
 
 export default Products;
