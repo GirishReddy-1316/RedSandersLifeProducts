1718b97c1e9a88783a35abcc2f66ad86a42c423f
GiriGunduluruTR <127122389+GiriGunduluruTR@users.noreply.github.com>
2024-04-18 21:04:01 +0530

    About page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -12,24 +12,24 @@ function About() {
         <section className="about-intro">
           <h1>About Jiyaba </h1>
           <p className="tagline">
-          A Taste of Authenticity, Straight from the Roots of the Red Sanders Tree to Your Bottle.
+          A Taste of Authenticity, Straight from the Roots of the Red Sanders Tree to Your Body.
           </p>
           <p>
-          We have held the license for Red sanders for over 5 years and have exported it to other countries, including Switzerland. Our goal is to purchase Red Sanders Trees from farmers who may not know how to sell them. We'll guide them through the process and buy directly from them.
+          Our company's story begins with a simple yet powerful belief: everyone deserves access to a healthy lifestyle and a shared commitment to making wellness accessible to all. With our dedication to quality, authenticity, and quantity, we aim to empower individuals to harness the potent properties of Red Sandalwood for their health and well-being. Join us in discovering the natural wonders of this ancient remedy and unlock a healthier, happier you.
           </p>
         </section>
 
         <section className="our-mission">
           <h2>Our Mission</h2>
           <p>
-          At Jiyaba, we believe in the potential of Red Sanders Trees and their benefits. We aim to introduce a range of products made from red sandalwood to provide various health benefits to people. Our goal is to offer a variety of products derived from Red Sanders Trees.
+          At JiYaBa, we're dedicated to sharing the incredible health benefits of Red Sandalwood by sourcing only the highest quality products from trusted suppliers. We ensure our customers receive the purest form of this plant. Plus, we prioritize ample availability so everyone can experience its full benefits for skincare, natural drinks, aromatherapy, and more. Trust us to provide reliable Red Sandalwood for your wellness needs.
           </p>
         </section>
 
         <section className="our-products">
           <h2>Our Products</h2>
           <p>
-          Discover our wide selection of products, including Pure Red Sanders Water, Red Sanders Water, Red Sandal Tea, Red Sandal Lipstick, Red Sandal Lip Balm, Red Sandal Sticks for Homam, Red Sandal Incense Sticks, and Red Sandalwood Powder for Face Packs, and more.
+          Explore our wide range of offerings, from Pure Red Sanders Water to Red Sandal cosmetics like lipstick and lip balm, along with Red Sandalwood Powder for face packs, Red Sandal Sticks for Yagna, and Red Sandal incense sticks. Additionally, we offer a selection of refreshing cool drinks and soft drinks to enhance your wellness routine.
           </p>
         </section>
 
@@ -38,7 +38,7 @@ function About() {
           <p>
             Want to learn more or get in touch with us? Visit our{" "}
             <a href="/contact">Contact Page</a> and let's start a conversation.
-            We're always here to talk About Red Sandal.
+            We're eager to chat about all things about our products.
           </p>
         </section>
       </main>
