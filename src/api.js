82f822fb2af00e73753b10d81ff08d81d1debe70
Rohit Sahu <sahu86744@gmail.com>
2024-05-12 17:22:32 +0530

    updated sever link

--- src/api.js
+++ src/api.js
@@ -1,14 +1,14 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: `https://sore-tan-pangolin-kilt.cyclic.app/api`,
+    baseURL: `https://redsanderslifeserver.onrender.com/api`,
     headers: {
         'Content-Type': 'application/json'
     }
 });
 
 const axiosInstanceWithToken = axios.create({
-    baseURL: "https://sore-tan-pangolin-kilt.cyclic.app/api",
+    baseURL: "https://redsanderslifeserver.onrender.com/api",
     headers: {
         "authorization": `Bearer ${localStorage.getItem("token")}`
     }
