816c5b20dabd78ae91ab1a60047406285ffccc3a
GiriGunduluruTR <gundulurugiri@gmail.com>
2024-05-13 20:35:34 +0530

    latest code

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -99,7 +99,7 @@ function Account() {
   async function handlerGoogleAuth() {
     try {
       setLoading(true);
-      window.location.href = `https://redsanderslifeserver.onrender.com/auth/google/callback`;
+      window.location.href = `https://redsanderslifeserver.onrender.com/auth/google`;
     } catch (error) {
       console.error(
         "Google login error:",
