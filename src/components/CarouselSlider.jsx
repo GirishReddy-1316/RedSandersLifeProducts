35d2058fdd4baba77424f92948ba7784987f7091
GiriGunduluruTR <gundulurugiri@gmail.com>
2024-05-13 15:26:05 +0530

    added home page css

--- src/components/CarouselSlider.jsx
+++ src/components/CarouselSlider.jsx
@@ -3,11 +3,10 @@ import Slider from "react-slick";
 import { Link } from "react-router-dom";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import "../styles/carousel.css";
 import slide1 from "../assets/slide1.jpg";
 import slide2 from "../assets/slide2.jpg";
 import slide3 from "../assets/slide3.jpg";
-import "../styles/carousel.css";
-
 function CarouselSlider() {
   const settings = {
     dots: true,
@@ -42,17 +41,73 @@ function CarouselSlider() {
     <Slider {...settings}>
       <Link to="/all-products">
         <div className="carousel-slide slideOne">
-          <h2 className="alignCenter">will show some content related to Jiyaba bevarages</h2>
+          <div className="upperSection">
+            <h2>
+              Enjoy nature's goodness with our healthy drinks made of Pure Red
+              Sandal!
+            </h2>
+            <h2>
+              We offer refreshing drinks and immune-boosting tonics for every
+              taste and lifestyle.
+            </h2>
+          </div>
+          <div className="row">
+            <div className="leftSideSection">
+              <img src={slide1} alt="jiyaba bevareges" />
+            </div>
+            <div className="rightSideSection">
+              <ul>
+                <li>UPGRADE YOUR HYDRATION TODAY!!</li>
+              </ul>
+            </div>
+          </div>
         </div>
       </Link>
       <Link to="/all-products">
         <div className="carousel-slide slideTwo">
-          <h2 className="alignCenter">will show some content related to Redsandal face powder</h2>
+          <div className="upperSection">
+            <h2>
+            Enhance your beauty the natural way with our pure red sandalwood face pack! 
+
+            </h2>
+            <h2>
+            Unveil radiant skin with the power of nature. Try it today for a glowing complexion!.
+            </h2>
+          </div>
+          <div className="row">
+            <div className="leftSideSection">
+              <img src={slide2} alt="jiyaba bevareges" />
+            </div>
+            <div className="rightSideSection">
+              <ul>
+                <li>TRY IT NOW FOR RADIANT SKIN!!</li>
+              </ul>
+            </div>
+          </div>
         </div>
       </Link>
       <Link to="/all-products">
         <div className="carousel-slide slideThree">
-          <h2 className="alignCenter">will show some content related to Jiyaba Pooja items</h2>
+        <div className="upperSection">
+            <h2>
+            Elevate your worship experience with our pure, genuine red sandalwood Homam sticks, Incense sticks and
+ 
+ Red sandal pearls
+            </h2>
+            <h2>
+            Immerse yourself in divine worship like never before…
+            </h2>
+          </div>
+          <div className="row">
+            <div className="leftSideSection">
+              <img src={slide3} alt="jiyaba bevareges" />
+            </div>
+            <div className="rightSideSection">
+              <ul>
+                <li>UPGRADE YOUR Pooja TODAY!!</li>
+              </ul>
+            </div>
+          </div>
         </div>
       </Link>
     </Slider>
