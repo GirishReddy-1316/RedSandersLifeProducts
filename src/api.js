2c42e058462d686d74e21cdfa01a043902d856fe
Rohit Sahu <sahu86744@gmail.com>
2024-05-26 18:04:57 +0530

    added backend url

--- src/api.js
+++ src/api.js
@@ -1,14 +1,14 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: `https://redsanderslifeserver.onrender.com/api`,
+    baseURL: `https://api.jiyaba.com/api`,
     headers: {
         'Content-Type': 'application/json'
     }
 });
 
 const axiosInstanceWithToken = axios.create({
-    baseURL: "https://redsanderslifeserver.onrender.com/api",
+    baseURL: "https://api.jiyaba.com/api",
     headers: {
         "authorization": `Bearer ${localStorage.getItem("token")}`
     }
