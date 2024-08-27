d2e77cf22f97b3c9b4030f7ace5a7cdba313abc6
GiriGunduluruTR <gundulurugiri@gmail.com>
2024-08-27 20:51:24 +0530

    updated contact us and home page

--- src/components/CarouselSlider.jsx
+++ src/components/CarouselSlider.jsx
@@ -43,33 +43,6 @@ function CarouselSlider() {
         <div className="carousel-slide slideOne">
           <div className="upperSection">
             <h2>
-              Enjoy nature's goodness with our healthy drinks made of Pure Red
-              Sandal!
-            </h2>
-            <h2>
-              We offer refreshing drinks and immune-boosting tonics for every
-              taste and lifestyle.
-            </h2>
-          </div>
-          <div className="row">
-            <div className="leftSideSection">
-              <img src={slide1} alt="jiyaba bevareges" />
-            </div>
-            <div className="rightSideSection">
-              <ul>
-                <li>UPGRADE YOUR HYDRATION TODAY!!</li>
-              </ul>
-              <button className="btn">
-                <i className="animation"></i>Shop Now<i className="animation"></i>
-              </button>
-            </div>
-          </div>
-        </div>
-      </Link>
-      <Link to="/all-products">
-        <div className="carousel-slide slideTwo">
-          <div className="upperSection">
-            <h2>
               Enhance your beauty the natural way with our pure red sandalwood
               face pack!
             </h2>
@@ -87,7 +60,36 @@ function CarouselSlider() {
                 <li>TRY IT NOW FOR RADIANT SKIN!!</li>
               </ul>
               <button className="btn">
-                <i className="animation"></i>Shop Now<i className="animation"></i>
+                <i className="animation"></i>Shop Now
+                <i className="animation"></i>
+              </button>
+            </div>
+          </div>
+        </div>
+      </Link>
+      <Link to="/all-products">
+        <div className="carousel-slide slideTwo">
+          <div className="upperSection">
+            <h2>
+              Enjoy nature's goodness with our healthy drinks made of Pure Red
+              Sandal!
+            </h2>
+            <h2>
+              We offer refreshing drinks and immune-boosting tonics for every
+              taste and lifestyle.
+            </h2>
+          </div>
+          <div className="row">
+            <div className="leftSideSection">
+              <img src={slide1} alt="jiyaba bevareges" />
+            </div>
+            <div className="rightSideSection">
+              <ul>
+                <li>UPGRADE YOUR HYDRATION TODAY!!</li>
+              </ul>
+              <button className="btn">
+                <i className="animation"></i>Shop Now
+                <i className="animation"></i>
               </button>
             </div>
           </div>
@@ -108,7 +110,8 @@ function CarouselSlider() {
             </div>
             <div className="rightSideSection">
               <button className="btn">
-                <i className="animation"></i>Buy Now<i className="animation"></i>
+                <i className="animation"></i>Buy Now
+                <i className="animation"></i>
               </button>
             </div>
           </div>
